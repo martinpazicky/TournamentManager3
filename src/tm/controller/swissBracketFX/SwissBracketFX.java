73ae98e39d4f366ac28934168c82864b47c4c687
xberesik <romiberko@gmail.com>
2021-04-18 19:59:29 +0200

    SwissSystem - fix non numeric and empty input

--- src/tm/controller/swissBracketFX/SwissBracketFX.java
+++ src/tm/controller/swissBracketFX/SwissBracketFX.java
@@ -34,13 +34,26 @@ public class SwissBracketFX extends AnchorPane {
     }
 
     public void setResult(){
-        swissBracket.getMatch().setParticipant1Score(Integer.valueOf(participant1Score.getText()));
-        swissBracket.getMatch().setParticipant2Score(Integer.valueOf(participant2Score.getText()));
-//        swissSystemController.addPoints(swissBracket);
+        String score1 = participant1Score.getText();
+        String score2 = participant2Score.getText();
+        if(score1 != null && !score1.trim().isEmpty() && score2 != null && !score2.trim().isEmpty() && isNumeric(score1) && isNumeric(score2))
+        {
+            swissBracket.getMatch().setParticipant1Score(Integer.valueOf(participant1Score.getText()));
+            swissBracket.getMatch().setParticipant2Score(Integer.valueOf(participant2Score.getText()));
+        }
     }
 
     public Button getSetResultBtn() {
         return setResultBtn;
+    }
+
+    public static boolean isNumeric(String str) {
+        try {
+            Double.parseDouble(str);
+            return true;
+        } catch(NumberFormatException e){
+            return false;
+        }
     }
 
     public SwissBracket getSwissBracket() {
